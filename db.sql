f24f4dc50bab7463f1ddc8dc0fe693e58e7716b8
Pedro Makengo <pedromakengomiguel@gmail.com>
2021-07-29 15:53:30 -0300

    Criando o banco de dados

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE estagio;
+
+CREATE TABLE tb_admin();
+CREATE TABLE tb_user();
+CREATE TABLE tb_company();
+CREATE TABLE tb_vacancy();
+CREATE TABLE tb_task();
+CREATE TABLE tb_task_acepted();
+CREATE TABLE tb_performance();
+CREATE TABLE tb_interview_user();
