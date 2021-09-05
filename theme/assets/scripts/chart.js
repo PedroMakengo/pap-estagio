9d0b266d30fd4638f31c0a04f1c44d4f27b9c5a2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-05 04:17:05 -0300

    Terminando o dashboard

--- theme/assets/scripts/chart.js
+++ theme/assets/scripts/chart.js
@@ -1,6 +1,6 @@
 var lineChart = document.getElementById("mycompra-chart").getContext("2d");
 var myLineChart = new Chart(lineChart, {
-  type: "bar",
+  type: "line",
   data: {
     labels: [
       "Jan",
@@ -19,14 +19,14 @@ var myLineChart = new Chart(lineChart, {
     datasets: [
       {
         label: "Empresas registadas",
-        borderColor: "#383891",
-        pointBorderColor: "#383891",
-        pointBackgroundColor: "#383891",
+        borderColor: "#1f6feb",
+        pointBorderColor: "#1f6feb",
+        pointBackgroundColor: "#1f6feb",
         pointBorderWidth: 2,
         pointHoverRadius: 4,
         pointHoverBorderWidth: 1,
         pointRadius: 4,
-        backgroundColor: "#383891",
+        backgroundColor: "transparent",
         fill: true,
         borderWidth: 2,
         data: [10, 20, 40, 10, 30, 10, 20, 30, 60, 20, 10, 20],
@@ -40,7 +40,7 @@ var myLineChart = new Chart(lineChart, {
       position: "bottom",
       labels: {
         padding: 10,
-        fontColor: "#1d7af3",
+        fontColor: "#1f6feb",
       },
     },
     tooltips: {
