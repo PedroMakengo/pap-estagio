246adc62bf1d039bb92d0581b012e26e7e9098d3
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-23 15:35:43 -0300

    Trabalhando na inserção de dados

--- source/controls/Create.php
+++ source/controls/Create.php
@@ -10,7 +10,19 @@
     $senha = md5(md5($_POST['senha']));
 
     if($_POST['tipo_usuario'] ==  'Empresa'):
-      echo "<script>window.alert('Criar usuário empresa');</script>";
+      // Inserir dados na tabela de uma empresa
+      $parametros = [
+        ":nome"     => $nome,
+        ":email"    => $email,
+        ":senha"    => $senha
+      ];
+      $inserirEmpresa = new Model();
+      $inserirEmpresa->EXE_NON_QUERY("INSERT INTO tb_empresa (nome_empresa, email_empresa, senha_empresa)
+      VALUES (:nome, :email, :senha) ", $parametros);
+      if($inserirEmpresa):
+        echo "<script>window.alert('Usuário inserido com sucesso');</script>";
+      endif;
+      // Verificação do tipo de usuário
     elseif($_POST['tipo_usuario'] == 'Estudante'):
         echo "<script>window.alert('Criar usuário empresa');</script>";
     endif;
