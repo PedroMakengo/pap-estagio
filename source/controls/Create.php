49c5b8fd603251e2bd2fe210a397a877bec16a35
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-23 15:39:58 -0300

    Trabalhando na inserção de empresas

--- source/controls/Create.php
+++ source/controls/Create.php
@@ -9,21 +9,27 @@
     $email = $_POST['email'];
     $senha = md5(md5($_POST['senha']));
 
+    $parametros = [
+      ":nome"     => $nome,
+      ":email"    => $email,
+      ":senha"    => $senha
+    ];
+
     if($_POST['tipo_usuario'] ==  'Empresa'):
       // Inserir dados na tabela de uma empresa
-      $parametros = [
-        ":nome"     => $nome,
-        ":email"    => $email,
-        ":senha"    => $senha
-      ];
       $inserirEmpresa = new Model();
       $inserirEmpresa->EXE_NON_QUERY("INSERT INTO tb_empresa (nome_empresa, email_empresa, senha_empresa)
       VALUES (:nome, :email, :senha) ", $parametros);
       if($inserirEmpresa):
-        echo "<script>window.alert('Usuário inserido com sucesso');</script>";
+        echo "<script>location.href='theme/company/home.php?id=home'</script>";
       endif;
       // Verificação do tipo de usuário
-    elseif($_POST['tipo_usuario'] == 'Estudante'):
-        echo "<script>window.alert('Criar usuário empresa');</script>";
+      elseif($_POST['tipo_usuario'] == 'Estudante'):
+        $inserirEstudante = new Model();
+        $inserirEstudante->EXE_NON_QUERY("INSERT INTO tb_aluno
+        (nome, numero_processo, email, senha) VALUES (:nome, '', :email, :senha)", $parametros);
+        if($inserirEstudante):
+          echo "<script>location.href='theme/study/home.php?id=home'</script>";
+        endif;
     endif;
   endif;
