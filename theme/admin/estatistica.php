ea5b2e713c79c404f57d8ac426f54a0301de37d5
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-04-18 17:13:10 -0300

    Estátisticas

--- theme/admin/estatistica.php
+++ theme/admin/estatistica.php
@@ -32,113 +32,26 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card card-static-1 shadow">
-                                    <div class="card-body">
-                                        <h5 class="text-muted">Total Empresas</h5>
-                                        <div class="metric-value d-inline-block">
-                                            <h1 class="mb-1">
-                                                <!-- Contar quantas empresas estão no sistema -->
-                                                <?php
-                                                  $contEmpresa = new Model();
-                                                  $empresas = $contEmpresa->EXE_QUERY("SELECT * FROM tb_empresa");
-                                                  echo count($empresas);
-                                                  ?>
-                                                <!-- Contar quantas empresas estão no sistema -->
-                                            </h1>
-                                        </div>
-                                        <div
-                                            class="metric-label d-inline-block float-right text-success font-weight-bold">
-                                            <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
-                                        </div>
-                                    </div>
-                                    <div id="sparkline-revenue"></div>
-                                </div>
+                          <div class="col-lg-6">
+                            <div class="card p-4">
+                              <h2>Gráfico</h2>
                             </div>
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card card-static-2 shadow">
-                                    <div class="card-body">
-                                        <h5 class="text-muted">Total de Estudantes</h5>
-                                        <div class="metric-value d-inline-block">
-                                            <h1 class="mb-1">
-                                              <!-- Contar quantas empresas estão no sistema -->
-                                              <?php
-                                                  $contAluno = new Model();
-                                                  $aluno = $contAluno->EXE_QUERY("SELECT * FROM tb_aluno");
-                                                  echo count($aluno);
-                                                  ?>
-                                                <!-- Contar quantas empresas estão no sistema -->
-                                            </h1>
-                                        </div>
-                                        <div
-                                            class="metric-label d-inline-block float-right text-success font-weight-bold">
-                                            <span><i class="fa fa-fw fa-arrow-up"></i></span><span>5.86%</span>
-                                        </div>
-                                    </div>
-                                    <div id="sparkline-revenue2"></div>
-                                </div>
+                          </div>
+                          <div class="col-lg-6">
+                            <div class="card p-4">
+                              <h2>Gráfico</h2>
                             </div>
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card card-static-3 shadow">
-                                    <div class="card-body">
-                                        <h5 class="text-muted">Genero (Masculino)</h5>
-                                        <div class="metric-value d-inline-block">
-                                            <h1 class="mb-1">
-                                              <!-- Contar quantas empresas estão no sistema -->
-                                              <?php
-                                                  $contAlunoMasculino = new Model();
-                                                  $alunoMasculino = $contAlunoMasculino->EXE_QUERY("SELECT * FROM tb_aluno WHERE sexo='M' ");
-                                                  echo count($alunoMasculino);
-                                                  ?>
-                                              <!-- Contar quantas empresas estão no sistema -->
-                                            </h1>
-                                        </div>
-                                        <div
-                                            class="metric-label d-inline-block float-right text-primary font-weight-bold">
-                                            <span>N/A</span>
-                                        </div>
-                                    </div>
-                                    <div id="sparkline-revenue3"></div>
-                                </div>
+                          </div>
+                          <div class="col-lg-6">
+                            <div class="card p-4">
+                              <h2>Gráfico</h2>
                             </div>
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card card-static-4 shadow">
-                                    <div class="card-body">
-                                        <h5 class="text-muted">Genero (Feminino)</h5>
-                                        <div class="metric-value d-inline-block">
-                                            <h1 class="mb-1">
-                                              <!-- Contar quantas empresas estão no sistema -->
-                                              <?php
-                                                  $contAlunoFeminino = new Model();
-                                                  $alunoFeminino = $contAlunoFeminino->EXE_QUERY("SELECT * FROM tb_aluno WHERE sexo='F' ");
-                                                  echo count($alunoFeminino);
-                                                  ?>
-                                              <!-- Contar quantas empresas estão no sistema -->
-                                            </h1>
-                                        </div>
-                                        <div
-                                            class="metric-label d-inline-block float-right text-secondary font-weight-bold">
-                                            <span>-2.00%</span>
-                                        </div>
-                                    </div>
-                                    <div id="sparkline-revenue4"></div>
-                                </div>
+                          </div>
+                          <div class="col-lg-6">
+                            <div class="card p-4">
+                              <h2>Gráfico</h2>
                             </div>
-                        </div>
-
-                        <div class="row">
-                            <!-- ============================================================== -->
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="card shadow">
-                                    <h5 class="card-header"><strong>Empresas registradas</strong></h5>
-                                    <div class="card-body p-0">
-                                       <div>
-                                            <canvas id="mycompra-chart" style="height: 300px"></canvas>
-                                       </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- ============================================================== -->
+                          </div>
                         </div>
                     </div>
                 </div>
