0c7f750a5ca172b16eb636b62bc8147db32789da
PedroMakengo <pedromakengomiguel@gamil.com>
2022-05-21 10:33:53 -0300

    Trabalhando

--- login.php
+++ login.php
@@ -25,43 +25,43 @@
 </head>
 <body>
     <main>
-        <div class="row limit-of-line">
-            <div class="col-xl-4 forms">
-                <form method="POST">
-                    <div class="col-lg-12 mb-5">
-                        <a href="index.php"><h1>Meu Estágio</h1></a>
-                        <p>Faça o seu logon dentro da nossa plataforma</p>
-                        <hr>
-                    </div>
-                    <div class="mt-3 col-lg-12 form-group">
-                        <input type="email" name="email" placeholder="Ex: eduardo@gmail.com" class="form-control form-control-lg">
-                    </div>
-                    <div class="mt-3 col-lg-12 form-group">
-                        <input type="password" name="password" placeholder="Password" class="form-control-lg form-control">
-                    </div>
-                    <div class="mt-3 col-lg-12">
-                        <input type="submit" value="Login" name="login" class="form-control-lg form-control">
-                    </div>
+      <div class="row limit-of-line">
+        <div class="col-xl-4 forms">
+          <form method="POST">
+              <div class="col-lg-12 mb-5">
+                  <a href="index.php"><h1>Meu Estágio</h1></a>
+                  <p>Faça o seu logon dentro da nossa plataforma</p>
+                  <hr>
+              </div>
+              <div class="mt-3 col-lg-12 form-group">
+                  <input type="email" name="email" placeholder="Ex: eduardo@gmail.com" class="form-control form-control-lg">
+              </div>
+              <div class="mt-3 col-lg-12 form-group">
+                  <input type="password" name="password" placeholder="Password" class="form-control-lg form-control">
+              </div>
+              <div class="mt-3 col-lg-12">
+                  <input type="submit" value="Login" name="login" class="form-control-lg form-control">
+              </div>
 
-                    <div class="col-lg-12 mt-4">
-                        <div class="row">
-                            <div class="col-lg-12 text-center">
-                                <p>Já tenho uma conta <a href="create.php">Criar conta</a></p>
-                            </div>
-                        </div>
-                    </div>
+              <div class="col-lg-12 mt-4">
+                  <div class="row">
+                      <div class="col-lg-12 text-center">
+                          <p>Já tenho uma conta <a href="create.php">Criar conta</a></p>
+                      </div>
+                  </div>
+              </div>
 
 
-                    <!-- Adicionando a configuração do login -->
-                    <?php
-                      require './source/controls/Login.php';
-                    ?>
-                    <!-- End Configuração do login -->
-                </form>
-            </div>
-            <div class="col-xl-8 background-login">
-            </div>
+              <!-- Adicionando a configuração do login -->
+              <?php
+                require './source/controls/Login.php';
+              ?>
+              <!-- End Configuração do login -->
+          </form>
         </div>
+        <div class="col-xl-8 background-login">
+        </div>
+      </div>
     </main>
 </body>
 </html>
--- source/controls/Login.php
+++ source/controls/Login.php
@@ -1,65 +1,69 @@
 <?php
   // Efectuando a verificação do login
-  if(isset($_POST['login'])){
+  if(isset($_POST['login'])):
     // Pegar os dados enviado apartir do front-end
-    $email = $_POST['email'];
-    $pass  = $_POST['password'];
+    if(empty($_POST['email']) && empty($_POST['password'])):
+      echo "<script>window.alert('Por favor preenche todos os campos')</script>";
+    else:
+      $email = $_POST['email'];
+      $pass  = $_POST['password'];
 
-    $parametro = [
-      ":email"  => $email,
-      ":senha"   => md5(md5($pass))
-    ];
-    // (1) Efectuando o login do Administrador
-    $login = new Model();
-    $loginAdmin = $login->EXE_QUERY("SELECT * FROM tb_admin WHERE email_admin=:email AND senha_admin=:senha", $parametro);
-    if($loginAdmin){
-      // Sessão do administrador
-      foreach($loginAdmin as $mostrar):
-        $_SESSION['id']       = addslashes($mostrar['id_admin']);
-        $_SESSION['nome']     = addslashes($mostrar['nome_admin']);
-        $_SESSION['email']    = addslashes($mostrar['email_admin']);
-        $_SESSION['senha']    = addslashes($mostrar['senha_admin']);
-        $_SESSION['foto']     = addslashes($mostrar['foto_admin']);
-      endforeach;
-      echo "<script>location.href='theme/admin/home.php?id=home'</script>";
-
-    // ===========================================================================================================================
-    }else {
-      // (2) Efectuando o login da company
-      $loginCompany = $login->EXE_QUERY("SELECT * FROM tb_empresa WHERE email_empresa=:email AND senha_empresa=:senha", $parametro);
-      if($loginCompany){
-        foreach($loginCompany as $mostrar):
-          $_SESSION['id']       = addslashes($mostrar['id_empresa']);
-          $_SESSION['nome']     = addslashes($mostrar['nome_empresa']);
-          $_SESSION['email']    = addslashes($mostrar['email_empresa']);
-          $_SESSION['senha']    = addslashes($mostrar['senha_empresa']);
-          $_SESSION['nif']      = addslashes($mostrar['nif']);
-          $_SESSION['local']    = addslashes($mostrar['localizacao']);
-          $_SESSION['contacto'] = addslashes($mostrar['contacto']);
-          $_SESSION['data']     = addslashes($mostrar['data_registro_empresa']);
-          $_SESSION['responsa'] = addslashes($mostrar['responsavel_empresa']);
-          $_SESSION['foto']     = addslashes($mostrar['foto']);
+      $parametro = [
+        ":email"  => $email,
+        ":senha"   => md5(md5($pass))
+      ];
+      // (1) Efectuando o login do Administrador
+      $login = new Model();
+      $loginAdmin = $login->EXE_QUERY("SELECT * FROM tb_admin WHERE email_admin=:email AND senha_admin=:senha", $parametro);
+      if($loginAdmin):
+        // Sessão do administrador
+        foreach($loginAdmin as $mostrar):
+          $_SESSION['id']       = addslashes($mostrar['id_admin']);
+          $_SESSION['nome']     = addslashes($mostrar['nome_admin']);
+          $_SESSION['email']    = addslashes($mostrar['email_admin']);
+          $_SESSION['senha']    = addslashes($mostrar['senha_admin']);
+          $_SESSION['foto']     = addslashes($mostrar['foto_admin']);
         endforeach;
-        echo "<script>location.href='theme/company/home.php?id=home'</script>";
-      }else {
-        // Efectuando login do usuário estudante
-        $loginStudy = $login->EXE_QUERY("SELECT * FROM tb_aluno WHERE email=:email AND senha=:senha", $parametro);
-        if($loginStudy){
-          foreach($loginStudy as $mostrar):
-            $_SESSION['id']   = addslashes($mostrar['id_aluno']);
-            $_SESSION['nome'] = addslashes($mostrar['nome']);
-            $_SESSION['email'] = addslashes($mostrar['email']);
-            $_SESSION['senha'] = addslashes($mostrar['senha']);
-            $_SESSION['foto'] = addslashes($mostrar['foto']);
-            $_SESSION['sexo'] = addslashes($mostrar['sexo']);
-            $_SESSION['processo'] = addslashes($mostrar['numero_processo']);
+        echo "<script>location.href='theme/admin/home.php?id=home'</script>";
+
+      // ===========================================================================================================================
+      else:
+        // (2) Efectuando o login da company
+        $loginCompany = $login->EXE_QUERY("SELECT * FROM tb_empresa WHERE email_empresa=:email AND senha_empresa=:senha", $parametro);
+        if($loginCompany):
+          foreach($loginCompany as $mostrar):
+            $_SESSION['id']       = addslashes($mostrar['id_empresa']);
+            $_SESSION['nome']     = addslashes($mostrar['nome_empresa']);
+            $_SESSION['email']    = addslashes($mostrar['email_empresa']);
+            $_SESSION['senha']    = addslashes($mostrar['senha_empresa']);
+            $_SESSION['nif']      = addslashes($mostrar['nif']);
+            $_SESSION['local']    = addslashes($mostrar['localizacao']);
             $_SESSION['contacto'] = addslashes($mostrar['contacto']);
-            $_SESSION['estado_aluno'] = addslashes($mostrar['estado_aluno']);
+            $_SESSION['data']     = addslashes($mostrar['data_registro_empresa']);
+            $_SESSION['responsa'] = addslashes($mostrar['responsavel_empresa']);
+            $_SESSION['foto']     = addslashes($mostrar['foto']);
           endforeach;
-          echo "<script>location.href='theme/study/home.php?id=home'</script>";
-        }else {
-          echo "<script>window.alert('Este usuário não exixte')</script>";
-        }
-      }
-    }
-  }
+          echo "<script>location.href='theme/company/home.php?id=home'</script>";
+        else:
+          // Efectuando login do usuário estudante
+          $loginStudy = $login->EXE_QUERY("SELECT * FROM tb_aluno WHERE email=:email AND senha=:senha", $parametro);
+          if($loginStudy):
+            foreach($loginStudy as $mostrar):
+              $_SESSION['id']   = addslashes($mostrar['id_aluno']);
+              $_SESSION['nome'] = addslashes($mostrar['nome']);
+              $_SESSION['email'] = addslashes($mostrar['email']);
+              $_SESSION['senha'] = addslashes($mostrar['senha']);
+              $_SESSION['foto'] = addslashes($mostrar['foto']);
+              $_SESSION['sexo'] = addslashes($mostrar['sexo']);
+              $_SESSION['processo'] = addslashes($mostrar['numero_processo']);
+              $_SESSION['contacto'] = addslashes($mostrar['contacto']);
+              $_SESSION['estado_aluno'] = addslashes($mostrar['estado_aluno']);
+            endforeach;
+            echo "<script>location.href='theme/study/home.php?id=home'</script>";
+          else:
+            echo "<script>window.alert('Este usuário não exixte')</script>";
+          endif;
+        endif;
+      endif;
+    endif;
+  endif;
