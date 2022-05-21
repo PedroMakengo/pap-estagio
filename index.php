95e4905bda06c87599e51bda47f940b662e9a6b7
PedroMakengo <pedromakengomiguel@gamil.com>
2022-05-21 09:20:12 -0300

    Trabalhando

--- index.php
+++ index.php
@@ -4,31 +4,31 @@
 <body>
 
     <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 text-left">
-                    <h1>Meu Estágio</h1>
-                </div>
-                <div class="col-lg-8 text-right">
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="create.php">Criar conta</a></li>
-                        <li><a href="login.php">Login</a></li>
-                    </ul>
-                </div>
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-4 text-left">
+                <h1>Meu Estágio</h1>
+            </div>
+            <div class="col-lg-8 text-right">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="create.php">Criar conta</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
             </div>
         </div>
+      </div>
     </header>
 
    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8">
-                    <h1>Plataforma de <span>Estágio</span></h1>
-                    <p>Conhece agora a sua plataforma de estagio, uma solução para os estudantes angolanos do 13º ano.</p>
-                </div>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <h1>Plataforma de <span>Estágio</span></h1>
+            <p>Conhece agora a sua plataforma de estagio, uma solução para os estudantes angolanos do 13º ano.</p>
+          </div>
         </div>
+      </div>
    </main>
 
     <!-- Require -->
