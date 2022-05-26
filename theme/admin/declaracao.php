0e872af53399013c73babc8ca73cc8ecf1aa0d17
PedroMakengo <pedromakengomiguel@gamil.com>
2022-05-26 02:29:44 -0300

    Trabalhando

--- theme/admin/declaracao.php
+++ theme/admin/declaracao.php
@@ -136,6 +136,7 @@
                                                         <th class="border-0">Data da Emissão</th>
                                                         <th class="border-0">Estado</th>
                                                         <th class="border-0 text-center">Acções</th>
+                                                        h
                                                     </tr>
                                                 </thead>
                                                 <tbody>
